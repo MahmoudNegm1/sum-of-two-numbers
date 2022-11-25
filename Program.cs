7ef8880d745d348ce0c8659412cd4fe43ae1f881
MahmoudNegm1 <77124033+MahmoudNegm1@users.noreply.github.com>
2022-11-25 10:10:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine( " this is sum opertator ");
+Console.WriteLine(" Please , enter your first number ");
+ int FirstNumber = int.Parse( Console.ReadLine() );
+Console.WriteLine("please enter second number ");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine($"sum of {FirstNumber} and {secondNumber} = {FirstNumber + secondNumber}");
